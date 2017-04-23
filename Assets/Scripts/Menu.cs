5dcd6e829a200993bc4d2a792df6c31550e18a74
Momin Khan <foolmoron@gmail.com>
2017-04-23 15:44:16 -0400

    Menu transition

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    Animator animator;
+
+    public bool MenuActive;
+    bool prevActive;
+    
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void ToggleMenu() {
+        MenuActive = !MenuActive;
+    }
+
+    void Update() {
+        if (prevActive != MenuActive) {
+            animator.Play(MenuActive ? "MenuEnter" : "MenuExit");
+            prevActive = MenuActive;
+        }
+    }
+}
